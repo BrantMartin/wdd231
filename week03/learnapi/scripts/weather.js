5f8209fcb508cd0df510921be5599dd71e7f875d
Brant Martin <100722069+BrantMartin@users.noreply.github.com>
2026-05-29 15:48:03 -0700

    week03 chamber

---
+++ week03/learnapi/scripts/weather.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const myKey = "3a4307b2b153320074983c3b8f4773a0";
+const myLat = "49.75";
+const myLong = "6.64";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+
+async function apiFetch() {
+	try {
+		const response = await fetch(url);
+		if (response.ok) {
+			const data = await response.json();
+			console.log(data);
+            displayResults(data);
+		} else {
+			throw Error(await response.text());
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function displayResults(data) {
+	currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+	const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+	let desc = data.weather[0].description;
+	weatherIcon.setAttribute("src", iconsrc);
+	weatherIcon.setAttribute("alt", data.weather[0].description);
+	captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
